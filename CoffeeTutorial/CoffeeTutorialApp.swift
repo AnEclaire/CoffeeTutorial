c18b093d6aefc5e11a51a75e2d0aff5784ea892b
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-08 22:48:30 -0800

    Initial Commit

---
+++ CoffeeTutorial/CoffeeTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeTutorialApp.swift
+//  CoffeeTutorial
+//
+//  Created by Emma Gutierrez on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
